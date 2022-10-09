553c7888cb459a6ab6160b278cf5ffb697e96649
Fabio Guardado <47063285+FabioGuardado@users.noreply.github.com>
2022-10-09 16:03:05 -0600

    Layout components (#2)
    * :lipstick: Add Navbar
    
    * :lipstick: Separate Divider component
    
    * :lipstick: Add Toolsbar component
    
    * :lipstick: Add Workspace component
    
    * :lipstick: Update Divider width
    
    * :lipstick: Add Divider and Canvas components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Toolsbar from './components/Toolsbar/Toolsbar';
+import Workspace from './components/Workspace/Workspace';
 
 function App() {
-  return <h1>Diagram App</h1>;
+  return (
+    <>
+      <Navbar />
+      <Toolsbar />
+      <Workspace />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+
+import Divider from '../Divider/Divider';
+
+import './Navbar.css';
+
+const Navbar = () => {
+  const [isMenuArchivoOpened, setIsMenuArchivoOpened] = useState(false);
+  const [isMenuEditarOpened, setIsMenuEditarOpened] = useState(false);
+
+  const toggleMenuArchivoOpened = () =>
+    setIsMenuArchivoOpened(!isMenuArchivoOpened);
+  const toggleMenuEditarOpened = () =>
+    setIsMenuEditarOpened(!isMenuEditarOpened);
+
+  return (
+    <>
+      <nav className="navbar-container">
+        <div className="navbar-content">
+          <div className="navbar-logo">LOGO</div>
+          <div className="navbar-menu">
+            <div className="navbar-menu-element">
+              <div className="navbar-button-container">
+                <button
+                  className="navbar-button button-archivo"
+                  onClick={toggleMenuArchivoOpened}
+                >
+                  Archivo
+                </button>
+              </div>
+              <ul
+                className={`navbar-submenu ${
+                  isMenuArchivoOpened ? 'submenu-active' : 'submenu-hidden'
+                }`}
+              >
+                <li>Guardar como PDF</li>
+                <li>Guardar como JPG</li>
+              </ul>
+            </div>
+
+            <div className="navbar-menu-element">
+              <div className="navbar-button-container">
+                <button
+                  className="navbar-button button-editar"
+                  onClick={toggleMenuEditarOpened}
+                >
+                  Editar
+                </button>
+              </div>
+              <ul
+                className={`navbar-submenu ${
+                  isMenuEditarOpened ? 'submenu-active' : 'submenu-hidden'
+                }`}
+              >
+                <li>Seleccionar todo</li>
+                <li>Agrupar</li>
+                <li>Desagrupar</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <Divider />
+    </>
+  );
+};
+
+export default Navbar;
