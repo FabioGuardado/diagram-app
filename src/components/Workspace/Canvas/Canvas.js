b22742ee55491f001c09a02c34a97b31b01c9620
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-10-14 22:39:46 -0600

    :rocket: prepare to demo

--- src/components/Workspace/Canvas/Canvas.js
+++ src/components/Workspace/Canvas/Canvas.js
@@ -48,14 +48,6 @@ const Canvas = () => {
       canvas.current.clientHeight,
     );
 
-    cuadros[0].r1.push(cuadros[2]);
-    cuadros[4].r1.push(cuadros[2]);
-    cuadros[1].r1.push(cuadros[2]);
-    cuadros[2].r1.push(cuadros[3]);
-    cuadros[5].r1.push(cuadros[2]);
-    // cuadros[6].r1.push(cuadros[5]);
-    // cuadros[6].r1.push(cuadros[2]);
-
     const image = new Image(60, 45); // Using optional size for image
     image.onload = drawImageActualSize; // Draw when image has loaded
     image.src = 'pc.png';
@@ -82,42 +74,6 @@ const Canvas = () => {
     contexto.strokeStyle = 'blue';
     contexto.rect(x, y, w, h);
     contexto.stroke();
-
-    dibujarTexto({ x, y, text });
-    r1.map(cuadro => calcularLinea(info, cuadro));
-  };
-
-  const calcularLinea = (origen, destino) => {
-    if (!destino) return;
-
-    const { x: forma1X, y: forma1Y, w: origenW, h: origenH } = origen;
-    const { x: forma2X, y: forma2Y, w: destinoW, h: destinoH } = destino;
-    const linea = {
-      origenX: origenW / 2 + forma1X,
-      origenY: origenH / 2 + forma1Y,
-      destinoX: destinoW / 2 + forma2X,
-      destinoY: destinoH / 2 + forma2Y,
-    };
-
-    dibujarLinea(linea);
-  };
-
-  const dibujarLinea = linea => {
-    const { origenX, origenY, destinoX, destinoY } = linea;
-    contexto.strokeStyle = 'red';
-    contexto.beginPath();
-    contexto.moveTo(origenX, origenY);
-    contexto.lineTo(destinoX, destinoY);
-    contexto.lineWidth = '1.2';
-    contexto.cap = 'round';
-    contexto.stroke();
-  };
-
-  const dibujarTexto = punto => {
-    const { x, y, text } = punto;
-    contexto.font = '30px serif';
-    contexto.fillStyle = 'black';
-    contexto.fillText(text, x, y - 5);
   };
 
   // Identificar el evento clic en la figura
