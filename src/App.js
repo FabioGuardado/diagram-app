9b7a703cee5e51adbe97d23bd37189a2e3164932
Fabio Guardado <fabio.guardado2012@gmail.com>
2022-10-14 20:54:51 -0600

    :sparkles: Add global context for canvas

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Navbar from './components/Navbar/Navbar';
 import Toolsbar from './components/Toolsbar/Toolsbar';
 import Workspace from './components/Workspace/Workspace';
 
+import CanvasProvider from './context/CanvasContext/CanvasProvider';
+
 function App() {
   return (
-    <>
+    <CanvasProvider>
       <Navbar />
       <Toolsbar />
       <Workspace />
-    </>
+    </CanvasProvider>
   );
 }
 
